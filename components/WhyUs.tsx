eabc7ca9ca6bebd8b45dc7f599003aa5908b6da9
MaxAnii <ansarbaba2000@gmail.com>
2024-10-04 18:03:47 +0530

    feat: creates the "why us" component for landing page.

---
+++ components/WhyUs.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WhyUs = () => {
+	return (
+		<div className="bg-white p-10 md:p-16">
+			<h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
+				Why Us?
+			</h2>
+			<p className="text-xl text-gray-700 text-center mx-auto md:w-2/4">
+				WhatsApp is a game changer. In India, WhatsApp is the go-to platform for
+				communication, boasting higher conversion rates than traditional
+				methods. With its vast user base, you can connect directly with your
+				audience, cutting through the noise of crowded platforms like YouTube.
+			</p>
+		</div>
+	);
+};
+
+export default WhyUs;
