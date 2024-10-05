60e605d3eee8baa1f4817fae41d1d3c10f044c6a
MaxAnii <ansarbaba2000@gmail.com>
2024-10-05 12:21:12 +0530

    Fix: fixes the ESLINT error.

--- components/Quotes.tsx
+++ components/Quotes.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Quotes = () => {
 	return (
 		<div className="bg-gray-100 p-10 md:p-16 ">
-			<h2 className="text-3xl md:text-4xl font-bold text-center mb-12">//</h2>
+			<h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
+				{"//"}
+			</h2>
 			<p className="text-lg text-gray-700 text-center mx-auto md:w-2/4 font-sans">
 				Using Eau has been a game-changer for our digital marketing strategy.
 				The insights we have gained have allowed us to refine our content and
