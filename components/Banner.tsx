b7978c74e8cac0ae23c98a64dd4a92fd1e1bba53
MaxAnii <ansarbaba2000@gmail.com>
2024-10-08 16:21:54 +0530

    stlye: changes the banner.

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,26 +1,65 @@
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import banner from "../Public/images/banner.png";
 import GetAccessButton from "./GetAccessButton";
+
 const Banner = () => {
 	return (
-		<div className="md:flex w-full  md:h-[80vh] items-center justify-center hero-section pb-5 mt-4">
-			<div className="flex  w-full items-center justify-center p-5 ">
-				<div className="text-center">
-					<h1 className="text-3xl font-normal lg:text-5xl text-wrap mb-4 ">
-						Drive more traffic to your videos with the help of WhatsApp.
-					</h1>
-					<div className="hidden md:block">
-						<GetAccessButton></GetAccessButton>
+		<div className="background  flex justify-center ">
+			<div className=" border-2 rounded-2xl bg-white	border-dotted md:w-[60vw] pt-16 pb-10 mb-5 mt-10 shadow-xl">
+				<h1 className="text-center text-4xl lg:text-5xl mb-6 mt-16 p-4 font-semibold leading-tight">
+					Drive More Traffic To Your YouTube Videos Via WhatsApp
+					<span className="inline-block bg-slate-200 text-5xl rounded-3xl p-3 ml-3 shadow-2xl align-middle">
+						🚀
+					</span>
+				</h1>
+
+				<div className=" mt-3 text-gray-400	text-center text-lg ">
+					Discover highly targeted keyword suggestions based on user intent and
+					current market trends.
+				</div>
+				<div className="	flex justify-center w-full">
+					<GetAccessButton></GetAccessButton>
+				</div>
+				<div className="flex justify-center mt-6 items-center gap-3">
+					<div className="flex">
+						<Avatar className="-ml-2 first:ml-0 border-2 border-white">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+						<Avatar className="-ml-4 border-2 border-white">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+						<Avatar className="-ml-4 border-2 border-white">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+						<Avatar className="-ml-4 border-2 border-white">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+						<Avatar className="-ml-4 border-2 border-white">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+					</div>
+					<div className="text-gray-400">500+ people wait for launch</div>
+				</div>
+
+				<div className=" rounded-2xl md:mr-5 mx-3 md:mx-0  flex justify-center mb-5 float-animation mt-11">
+					<div className="shadow-2xl border-8 border-zinc-200 rounded-2xl">
+						<div className="border-[5px] border-black  rounded-lg p-2">
+							<Image
+								src={banner}
+								alt="banner image"
+								width={700}
+								height={500}
+							></Image>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="shadow-2xl border-[8px] border-stone-500  rounded-2xl md:mr-5 mx-3 md:mx-0 md:w-full flex justify-center mb-5 float-animation">
-				<div className="border-[5px] border-black  rounded-lg p-2">
-					<Image src={banner} alt="banner image"></Image>
-				</div>
-			</div>
-			<div className=" md:hidden flex justify-center w-full">
-				<GetAccessButton></GetAccessButton>
 			</div>
 		</div>
 	);
